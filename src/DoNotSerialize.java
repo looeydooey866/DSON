8d9f5152d6564ed5c5dec8ce47a4b6bf728a18a3
looeydooey866 <looeydooey866@gmail.com>
2025-06-17 13:46:54 +0800

    Docs for annotations

--- src/DoNotSerialize.java
+++ src/DoNotSerialize.java
@@ -3,6 +3,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * An annotation to be added to object fields. If added to an object field, it will not be serialized into JSON strings by DamnSON.
+ * Fields annotated with DoNotSerialize will also not accept data from JSON.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface DoNotSerialize{}
--- src/Rename.java
+++ src/Rename.java
@@ -3,6 +3,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * An annotation to be added to object fields, to change its associated name for DamnSON parsing.
+ * To use, simply add the annotation as follows:
+ * <br><br>
+ * {@code @Rename("number")}
+ * <br>
+ * {@code public int N;}
+ * <br><br>
+ * When the field is parsed, it will be reflected as {@code number: ...} instead of {@code n (fields are lowercased): ...}.
+ * <br><br>
+ * When the field is serialized and data is read from JSON, the parser will look for the name {@code number} instead of {@code n}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Rename {
